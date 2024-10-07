ba5d82e5219d2192f0d39eb2763bc8b57996a324
Viennbalcita <vienn_rowen_balcita@dlsu.edu.ph>
2024-10-07 13:51:35 +0800

    Added int heuristic to Node.java

--- src/main/java/intsy/group7/pathfinder_sim/model/Node.java
+++ src/main/java/intsy/group7/pathfinder_sim/model/Node.java
@@ -4,14 +4,20 @@ import java.util.LinkedList;
 public class Node {
     private String id;
     private LinkedList<Edge> edges;
+    private int heuristic;
 
-public Node(String id){
+public Node(String id, int heuristic){
     this.id = id;
+    this.heuristic = heuristic;
     this.edges = new LinkedList<>();
 }
 
 public String getId(){
     return this.id;
+}
+
+public int getHeuristic(){
+    return this.heuristic;
 }
 
 public void setId(String id){
