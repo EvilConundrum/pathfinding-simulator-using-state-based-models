5630363f44ffc1e6c211a090c2ec8e65d45e72b6
jazjimenez <jaztin_jacob_jimenez@dlsu.edu.ph>
2024-10-07 13:16:45 +0800

    Implement Blind Search Algorithm(s) #5
    Implemented the BFS Algorithm

--- src/main/java/intsy/group7/pathfinder_sim/algorithm/BFSAlgorithm.java
+++ src/main/java/intsy/group7/pathfinder_sim/algorithm/BFSAlgorithm.java
@@ -1,4 +1,66 @@
 package intsy.group7.pathfinder_sim.algorithm;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import intsy.group7.pathfinder_sim.model.Graph;
+import intsy.group7.pathfinder_sim.model.Node;
+import intsy.group7.pathfinder_sim.model.Edge;
+
 public class BFSAlgorithm {
+
+    /**
+     * The algorithm implementation for BFS
+     * 
+     * @author Jaztin Jimenez
+     * @return the path of the BFS algorithm or null if path is not found
+     */
+    public static List<Node> bfs(Graph graph, Node start, Node goal) {
+    Queue<Node> queue = new LinkedList<>();
+    Set<Node> visited = new HashSet<>();
+    Map<Node, Node> parent = new HashMap<>();
+
+    queue.add(start);
+    visited.add(start);
+    parent.put(start, null);
+
+    while (!queue.isEmpty()) {
+        Node node = queue.poll();
+
+        if (node.equals(goal)) { // if goal node is found, construct the path for the BFS path output
+            return reconstructPath(parent, goal);
+        }
+
+        for (Edge edge : node.getEdges()) { // Goes through every child node of the parent node
+            Node neighbor = edge.getDestination();
+            if (!visited.contains(neighbor)) { // skips the node if the node is already visited
+                queue.add(neighbor);
+                visited.add(neighbor);
+                parent.put(neighbor, node);
+            }
+        }
+    }
+    
+    return null; // No Path found
+    
+}
+
+    /**
+     * Function to return the list of nodes that shows the path of the BFS algorithm
+     * 
+     * @author Jaztin Jimenez
+     * @return list of nodes that shows the path of the BFS algorithm
+     */
+    private static List<Node> reconstructPath(Map<Node, Node> parent, Node goal) {
+        List<Node> path = new LinkedList<>();
+        for (Node node = goal; node != null; node = parent.get(node)) {
+            path.add(0, node);
+        }
+        return path;
+    }
 }
