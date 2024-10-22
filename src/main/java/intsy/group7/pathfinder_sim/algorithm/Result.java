8abece2c72050c4f12d30d029cd8ee8b07ac62de
jazjimenez <jaztin_jacob_jimenez@dlsu.edu.ph>
2024-10-23 01:28:11 +0800

    Added pathCost to Result

--- src/main/java/intsy/group7/pathfinder_sim/algorithm/Result.java
+++ src/main/java/intsy/group7/pathfinder_sim/algorithm/Result.java
@@ -2,15 +2,18 @@ package intsy.group7.pathfinder_sim.algorithm;
 
 import java.util.List;
 
+import static intsy.group7.pathfinder_sim.algorithm.PathCostCalculator.calculateTotalCost;
 import intsy.group7.pathfinder_sim.model.Node;
 
 public class Result {
     private final List<Node> path;
     private final List<Node> traversal;
+    private final int pathCost;
 
     public Result(List<Node> path, List<Node> traversal) {
         this.path = path;
         this.traversal = traversal;
+        this.pathCost = calculateTotalCost(path);
     }
 
     public List<Node> getPath() {
@@ -19,5 +22,9 @@ public class Result {
 
     public List<Node> getTraversal() {
         return traversal;
+    }
+
+    public int getPathCost() {
+        return pathCost;
     }
 }
