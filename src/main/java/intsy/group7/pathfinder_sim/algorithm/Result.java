37c0e1cac65844d29b659ba5185e2f87e4632e58
SKD <seankyled1st@gmail.com>
2024-10-22 18:13:28 +0800

    Reverted back to original List Type

--- src/main/java/intsy/group7/pathfinder_sim/algorithm/Result.java
+++ src/main/java/intsy/group7/pathfinder_sim/algorithm/Result.java
@@ -1,25 +1,19 @@
 package intsy.group7.pathfinder_sim.algorithm;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import intsy.group7.pathfinder_sim.model.Node;
 
 public class Result {
-    private final LinkedList<Node> path;
+    private final List<Node> path;
     private final List<Node> traversal;
 
-    public Result(LinkedList<Node> path, List<Node> traversal) {
+    public Result(List<Node> path, List<Node> traversal) {
         this.path = path;
         this.traversal = traversal;
-
-        // DEBUGGING 
-        for (int i=0; i < path.size(); i++) {
-            System.out.print(path.get(i).getId() + " ");
-        }
     }
 
-    public LinkedList<Node> getPath() {
+    public List<Node> getPath() {
         return path;
     }
 
