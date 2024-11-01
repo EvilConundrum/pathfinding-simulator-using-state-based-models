59b7bb3d86104db332502f5d37b0d6b57745ce4f
Clive Ang <clive_jarel_c_ang@dlsu.edu.ph>
2024-11-01 09:06:07 +0800

    refactor: change csv file path to reflect earlier changes in project structure

--- src/main/java/intsy/group7/pathfinder_sim/Driver.java
+++ src/main/java/intsy/group7/pathfinder_sim/Driver.java
@@ -2,17 +2,17 @@ package intsy.group7.pathfinder_sim;
 
 import intsy.group7.pathfinder_sim.model.*;
 import intsy.group7.pathfinder_sim.dao.*;
-
 import intsy.group7.pathfinder_sim.controller.*;
 
 public class Driver{
     public static void main(String[] args) {
-        System.out.println("Test Print");
-        
-        Graph graph = new Graph(); // Main Modelw
-        GraphDAO graphDAO = new GraphDAO(); // File Reader for Nodes and Edges
-        graphDAO.loadGraphNodes("src/main/java/intsy/group7/pathfinder_sim/dao/nodes.csv", graph);
-        graphDAO.loadGraphEdges("src/main/java/intsy/group7/pathfinder_sim/dao/edges.csv", graph);
-        new MainController(graph); // Main Controller
+        Graph       graph = new Graph();                        // Main model
+        GraphDAO    graphDAO = new GraphDAO();                  // File reader for nodes and edges
+        String      CSVFolderPath = "src/main/resources/csv/";  // Relative path to the csv folder
+
+        graphDAO.loadGraphNodes(CSVFolderPath + "nodes.csv", graph);
+        graphDAO.loadGraphEdges(CSVFolderPath + "edges.csv", graph);
+
+        new MainController(graph);
     }
 }
