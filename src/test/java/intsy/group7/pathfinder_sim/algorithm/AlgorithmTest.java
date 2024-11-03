563342798a982304511cc7b0aa9efdb1b7f783a2
Clive Ang <clive_jarel_c_ang@dlsu.edu.ph>
2024-11-03 15:00:21 +0800

    build: add classes  to test time complexity of the different algorithms

---
+++ src/test/java/intsy/group7/pathfinder_sim/algorithm/AlgorithmTest.java
@@ -0,0 +1,8 @@
+package intsy.group7.pathfinder_sim.algorithm;
+
+import intsy.group7.pathfinder_sim.algorithm.result.Result;
+import intsy.group7.pathfinder_sim.model.Node;
+
+public interface AlgorithmTest {
+    public Result testTimeComplexity(Node start, Node goal);
+}
